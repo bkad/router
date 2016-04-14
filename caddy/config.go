0d91570cee8daec08967d3ac134c40cbdf8bc101
Matt Tucker <matt@newmediadenver.com>
2016-04-14 16:38:20 -0600

    template changes, adding a default placeholder, use apk install of caddy, and auto agree to letsencrypt.

--- caddy/config.go
+++ caddy/config.go
@@ -9,16 +9,17 @@ import (
 )
 
 const (
-	confTemplate = `{{ $routerConfig := . }}
-{{ range $appConfig := $routerConfig.AppConfigs }}{{ range $domain := $appConfig.Domains }}
-{{ if $appConfig.Available }}
+	confTemplate = `# Automatically generated Caddyfile
+0.0.0.0 {
+	root /opt/router/default
+}
+{{ $routerConfig := . }}
+{{ range $appConfig := $routerConfig.AppConfigs }}{{ range $domain := $appConfig.Domains }}{{ if $appConfig.Available }}
 {{ if contains "." $domain }}{{ $domain }}{{ else if ne $routerConfig.PlatformDomain "" }}{{ $domain }}.{{ $routerConfig.PlatformDomain }}{{ else }}{{ $domain }}{{ end }} {
 	proxy / {{$appConfig.ServiceIP}}:80
 	tls off
 }
-
-{{ end }}
-{{ end }}{{ end }}
+{{ end }}{{ end }}{{ end }}
 `
 )
 
